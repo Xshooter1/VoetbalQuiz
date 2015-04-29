6ba6d44a24d5dbfcd21dce2adc4b4d216779847e
Xshooter3D <rietvelddominique@gmail.com>
2015-04-29 12:47:00 +0200

    just pushing te basic setup

---
+++ src/soccerquiz/domain/Question.java
@@ -0,0 +1,14 @@
+/******************************************************************************\
+**                            Dominique Rietveld                              **
+**                            Student Avans Breda                             **
+\******************************************************************************/
+
+package soccerquiz.domain;
+
+/**
+ *
+ * @author Dominique Rietveld
+ */
+public class Question {
+
+}
---
+++ src/soccerquiz/main/SoccerQuiz.java
@@ -0,0 +1,21 @@
+/******************************************************************************\
+**                            Dominique Rietveld                              **
+**                            Student Avans Breda                             **
+\******************************************************************************/
+
+package soccerquiz.main;
+
+/**
+ *
+ * @author Dominique Rietveld
+ */
+public class SoccerQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
