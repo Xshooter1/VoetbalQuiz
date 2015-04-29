63b6453e4c806af6a5f8461f4757525c97ddfa56
Xshooter3D <rietvelddominique@gmail.com>
2015-04-29 12:52:28 +0200

    Question class has constructor

--- src/soccerquiz/domain/Question.java
+++ src/soccerquiz/domain/Question.java
@@ -10,5 +10,10 @@ package soccerquiz.domain;
  * @author Dominique Rietveld
  */
 public class Question {
-
+    private String stadium, club;
+    
+    public Question (String stadium, String club){
+        this.stadium = stadium;
+        this.club = club;
+    }
 }
